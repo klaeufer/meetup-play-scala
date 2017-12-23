629ff0b857ce9c1af51273ae0c6c64611c44331e
Konstantin Läufer <klaeufer@gmail.com>
2017-12-23 14:31:32 -0600

    got basic OAuth2 flow to work, fixes #3
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -25,7 +25,10 @@ class HomeController @Inject() (config: Configuration, cc: ControllerComponents,
 
   val logger = Logger(this.getClass)
 
-  val apiKey = config.get[String]("meetup.api-key")
+  val clientId = config.get[String](KeyClientId)
+  val clientSecret = config.get[String](KeyClientSecret)
+  logger.debug(s"$KeyClientId = $clientId")
+  logger.debug(s"$KeyClientSecret = $clientSecret")
 
   implicit val groupFormat = Json.format[Group]
   implicit val eventFormat = Json.format[Event]
@@ -44,12 +47,46 @@ class HomeController @Inject() (config: Configuration, cc: ControllerComponents,
     * will be called when the application receives a `GET` request with
     * a path of `/`.
     */
-  def index() = Action {
-    Ok(views.html.index())
+  def index() = Action { implicit request =>
+    request.session.get(KeyAccessToken) match {
+      case None =>
+        val authArgs = Seq(
+          "client_id" -> clientId,
+          "response_type" -> "code",
+          "redirect_uri" -> RedirectUrl
+        )
+        val authUrlWithArgs = ws.url(AuthUrl).addQueryStringParameters(authArgs: _*).uri
+        logger.debug(s"authUrl = $authUrlWithArgs")
+        Ok(views.html.index(authUrlWithArgs))
+      case Some(_) =>
+        Ok(views.html.effortQuery())
+    }
+  }
+
+  def auth(code: String) = Action.async {
+    val tokenArgs = Map(
+      "client_id" -> clientId,
+      "client_secret" -> clientSecret,
+      "grant_type" -> "authorization_code",
+      "redirect_uri" -> RedirectUrl,
+      "code" -> code
+    )
+    logger.debug(tokenArgs.toString)
+    ws.url(TokenUrl).post(tokenArgs).map { response =>
+      val json = Json.parse(response.body)
+      logger.debug(Json.prettyPrint(json))
+      val accessToken = json("access_token").as[String]
+      val refreshToken = json("refresh_token").as[String]
+      logger.debug(s"storing access and refresh tokens = $accessToken $refreshToken")
+      Ok(views.html.effortQuery()).withSession(KeyAccessToken -> accessToken)
+    }
   }
 
   def effort(from: Option[String], until: Option[String]) = Action.async { implicit request =>
-    val client = ws.url(ServiceUrl).addQueryStringParameters("key" -> apiKey)
+    val accessToken = request.session.get(KeyAccessToken).get
+    logger.debug(s"found access token $accessToken in session")
+    val authHeader = KeyAuthorization -> s"Bearer $accessToken"
+    val client = ws.url(ServiceUrl).addHttpHeaders(authHeader)
     logger.debug(s"submitting request to ${client.url}")
     client.get() map { response =>
       logger.debug(response.body)
@@ -67,16 +104,16 @@ class HomeController @Inject() (config: Configuration, cc: ControllerComponents,
             logger.debug(s"found ${events.length} events total")
             val result = Effort(events, interval)
             render {
-              case Accepts.Html() => Ok(views.html.index(result.toString))
+              case Accepts.Html() => Ok(views.html.message(result.toString))
               case _              => Ok(Json.toJson(result))
             }
           } getOrElse {
-            Ok(views.html.index("JSON parse error"))
+            Ok(views.html.message("JSON parse error"))
           }
-        case status => Ok(views.html.index(s"it didn't work: $status ${response.body}"))
+        case status => Ok(views.html.message(s"it didn't work: $status ${response.body}"))
       }
     } recover {
-      PartialFunction(_ => Ok(views.html.index("API request timeout")))
+      PartialFunction(_ => Ok(views.html.message("API request timeout")))
     }
   }
 }
--- app/controllers/controllers.scala
+++ app/controllers/controllers.scala
@@ -3,9 +3,9 @@ package object controllers {
   val AppVersion = "0.1"
 
   val PropFileName = "local.properties"
-  val KeyClientId = "MEETUP_CLIENT_ID"
-  val KeyClientSecret = "MEETUP_CLIENT_SECRET"
-  val KeyApiKey = "MEETUP_API_KEY"
+  val KeyClientId = "meetup.client-id"
+  val KeyClientSecret = "meetup.client-secret"
+  val KeyApiKey = "meetup.api-key"
   val KeyAccessToken = "accessToken"
   val KeyRefreshToken = "refreshToken"
   val KeyAuthorization = "Authorization"
@@ -13,9 +13,9 @@ package object controllers {
   val AuthUrl = "https://secure.meetup.com/oauth2/authorize"
   val TokenUrl = "https://secure.meetup.com/oauth2/access"
   val ServiceUrl = "https://api.meetup.com/self/events?desc=true"
-  val RedirectServerPort = 8080
+  val RedirectServerPort = 9000
   val RedirectServerAddress = "0.0.0.0"
-  val RedirectUrl = s"http://localhost:$RedirectServerPort"
-  val DefaultWebServerPort = "8080"
+  val RedirectUrl = s"http://localhost:$RedirectServerPort/auth"
+  val DefaultWebServerPort = "9000"
   val WebServerAddress = "0.0.0.0"
 }
